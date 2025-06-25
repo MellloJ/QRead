10f6b54786086ec0247f600d1cdb33999b3dc53b
MellloJ <jdesousamadeira@gmail.com>
2025-06-25 16:59:06 -0300

    Implementacao dos testes web

---
+++ mobile/qread-app/capacitor.config.ts
@@ -0,0 +1,16 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'qread-app',
+  webDir: 'www'
+};
+
+// const config: CapacitorConfig = {
+//     "appId": "com.qread.app",
+//     "appName": "QRead",
+//     "webDir": "www",
+//     "bundledWebRuntime": false
+// }
+
+export default config;
